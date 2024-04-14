03a0bd220afdf3dd52664bf995fc1a891b605c95
Jeong Deoksu <jeongdeoksu@gmail.com>
2024-04-14 11:40:54 +0900

    Add React.memo

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,4 +1,5 @@
 import "./TodoItem.css";
+import { memo } from "react";
 
 const TodoItem = ({ id, content, date, idDone, onUpdate, onDelete }) => {
   const onChangeCheckbox = () => {
@@ -24,4 +25,12 @@ const TodoItem = ({ id, content, date, idDone, onUpdate, onDelete }) => {
   );
 };
 
-export default TodoItem;
+export default memo(TodoItem, (prevProps, nextProps) => {
+  console.log(prevProps, nextProps);
+  if (prevProps.id !== nextProps.id) return false;
+  if (prevProps.idDone !== nextProps.idDone) return false;
+  if (prevProps.content !== nextProps.content) return false;
+  if (prevProps.date !== nextProps.date) return false;
+
+  return true;
+});
