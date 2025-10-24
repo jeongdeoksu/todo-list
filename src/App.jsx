d92e42ca0c9eea7c49722a2e6b63f5124043643a
jeongdeoksu <jeongdeoksu@gmail.com>
2025-10-24 13:38:07 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Header from "./components/Header.jsx";
+import Editor from "./components/Editor.jsx";
+import List from "./components/List.jsx";
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <Editor />
+            <List />
+        </div>
+    );
+}
+
+export default App
