075c56ea1e93d10fb5a08cacda8a5f433baabe79
jeongdeoksu <jeongdeoksu@gmail.com>
2025-11-07 20:30:44 +0900

    UI 구현

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,5 +1,12 @@
+import"./Editor.css"
+
 const Editor = () => {
-    return <div>Editor</div>
+    return (
+        <div className="Editor">
+            <input placeholder='새로운 Todo...'/>
+            <button>추가</button>
+        </div>
+    )
 }
 
 export default Editor;
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,18 @@
+import "./List.css"
+import TodoItem from "./TodoItem.jsx"
+
 const List = () => {
-    return <div>List</div>
+    return (
+      <div className="List">
+        <h4>Todo List</h4>
+        <input placeholder="검색어를 입력하세요"/>
+        <div className="todos_wrapper">
+          <TodoItem />
+          <TodoItem />
+          <TodoItem />
+        </div>
+      </div>
+    )
 }
 
 export default List;
