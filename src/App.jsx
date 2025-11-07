680bed24788fd765171e86e49ac847478977cda6
jeongdeoksu <jeongdeoksu@gmail.com>
2025-11-08 00:07:22 +0900

    기능 구현 준비

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
 import Header from "./components/Header.jsx";
 import Editor from "./components/Editor.jsx";
 import List from "./components/List.jsx";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 0,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+]
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
     return (
         <div className="App">
             <Header />
